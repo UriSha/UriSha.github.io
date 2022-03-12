e6ba083f706a306cb760fde1695dee1bb34fd328
UriSha <uri.shaham1@gmail.com>
2022-03-12 17:22:10 +0200

    update

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:/var/folders/k0/bwrg7sc578s7mtg877m1rlhw0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5@v5.0.0-20211231133358-6d4989f90f7a/assets/js/wowchemy-utils.js
+  // ns-hugo:/var/folders/jp/jr1f7_k9059bgqwv77hytzlr0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5@v5.0.0-20211231133358-6d4989f90f7a/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
